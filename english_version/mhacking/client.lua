637e430685ce54c9ee9511c99ba7d9b5cf0aeba9
Pringles <theyork342@hotmail.com>
2019-08-27 13:28:29 -0300

    all variables are now in english as well

--- english_version/mhacking/client.lua
+++ english_version/mhacking/client.lua
@@ -4,14 +4,15 @@
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- VARIABLES
 -----------------------------------------------------------------------------------------------------------------------------------------
-local carrof_hash = GetHashKey('stockade')
-local seguranca = GetHashKey("s_m_m_security_01")
-local carro_segurancas_hash = GetHashKey('cognoscenti2')
+local carf_hash = GetHashKey('stockade')
+local security = GetHashKey("s_m_m_security_01")
+local security_cars_hash = GetHashKey('cognoscenti2')
 
 local local_hack_x = 1275.682
 local local_hack_y = -1710.426
 local local_hack_z = 54.771
 local amount = 300000
+local hacking = false
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- HACKING
 -----------------------------------------------------------------------------------------------------------------------------------------
@@ -19,8 +20,8 @@ Citizen.CreateThread(function()
   while true do 
     Citizen.Wait(1)
       local ped = PlayerPedId()
-      local distancia_hack = GetDistanceBetweenCoords(GetEntityCoords(ped), local_hack_x, local_hack_y, local_hack_z)
-      if distancia_hack <=1 and not IsEntityDead(ped) then
+      local hack_distance = GetDistanceBetweenCoords(GetEntityCoords(ped), local_hack_x, local_hack_y, local_hack_z)
+      if hack_distance <=1 and not IsEntityDead(ped) and not hacking then
         drawTxt("PRESS  ~b~E~w~  TO START HACKING",4,0.5,0.93,0.50,255,255,255,180)
         if IsControlJustPressed(0, 38) and (UpdateOnscreenKeyboard() ~= 0)then
           RequestAnimDict('anim@heists@prison_heistig1_p1_guard_checks_bus')
@@ -28,7 +29,6 @@ Citizen.CreateThread(function()
 						Wait(10)
           end
           SetEntityHeading(ped,310.46)
-	  TaskPlayAnim(ped,'anim@heists@prison_heistig1_p1_guard_checks_bus','loop',8.0,8.0,-1,1,0,false,false,false)
           TaskPlayAnim(ped,'anim@heists@prison_heistig1_p1_guard_checks_bus','loop',8.0,8.0,-1,1,0,false,false,false)
           Wait(1500)
           DoScreenFadeOut(1000)
@@ -71,8 +71,8 @@ end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- CHECKING AND BUILDING
 -----------------------------------------------------------------------------------------------------------------------------------------
-function mycb(successo, temporestante)
-  if successo then
+function mycb(success, timeremaining)
+  if success then
     local ped = PlayerPedId()
     --TriggerEvent('chatMessage', "O lugar do carro forte está no seu gps, corra e intercepte-o.")
     TriggerEvent("Notify","sucesso","Sistema hackeado com sucesso, o carro forte esta saindo do banco 3.")
@@ -80,66 +80,67 @@ function mycb(successo, temporestante)
     DoScreenFadeIn(1000)
     Wait(1350)
     ClearPedTasks(ped)
-    setupModelo(carrof_hash)
-    setupModelo(carro_segurancas_hash)
-    local carrof_spw = CreateVehicle(carrof_hash, -2959.50, 492.623, 14.81, 88.75, true, false)
-    local carros_spw = CreateVehicle(carro_segurancas_hash, -2943.69, 492.69, 15.29, 88.75, true, false)  
-    local carros_spw_2 = CreateVehicle(carro_segurancas_hash, -2971.292,493.226,15.318, true, false)
-    carrosinfo(carrof_spw)
-    carrosinfo(carros_spw)
-    carrosinfo(carros_spw_2)
-    setupModelo(seguranca)
-    local seguranca_spw = CreatePedInsideVehicle(carrof_spw, 4, seguranca, -1, true, false)
-    local seguranca_spw_2 = CreatePedInsideVehicle(carrof_spw, 4, seguranca, 0, true, false)
-    local seguranca_spw_3 = CreatePedInsideVehicle(carros_spw, 4, seguranca, -1, true, false)
-    local seguranca_spw_4 = CreatePedInsideVehicle(carros_spw, 4, seguranca, 0, true, false)
-    local seguranca_spw_5 = CreatePedInsideVehicle(carros_spw, 4, seguranca, 1, true, false)
-    local seguranca_spw_6 = CreatePedInsideVehicle(carros_spw, 4, seguranca, 2, true, false)
-    local seguranca_spw_7 = CreatePedInsideVehicle(carros_spw_2, 4, seguranca, -1, true, false)
-    local seguranca_spw_8 = CreatePedInsideVehicle(carros_spw_2, 4, seguranca, 0, true, false)
-    local seguranca_spw_9 = CreatePedInsideVehicle(carros_spw_2, 4, seguranca, 1, true, false)
-    local seguranca_spw_10 = CreatePedInsideVehicle(carros_spw_2, 4, seguranca, 2, true, false)
-    SetEntityAsMissionEntity(seguranca_spw, 0, 0) 
-    SetEntityAsMissionEntity(seguranca_spw_2, 0, 0)
-    SetEntityAsMissionEntity(seguranca_spw_3, 0, 0) 
-    SetEntityAsMissionEntity(seguranca_spw_4, 0, 0) 
-    SetEntityAsMissionEntity(seguranca_spw_5, 0, 0)
-    SetEntityAsMissionEntity(seguranca_spw_6, 0, 0)
-    SetEntityAsMissionEntity(seguranca_spw_7, 0, 0)
-    SetEntityAsMissionEntity(seguranca_spw_8, 0, 0)
-    SetEntityAsMissionEntity(seguranca_spw_9, 0, 0)
-    SetEntityAsMissionEntity(seguranca_spw_10, 0, 0) 
-    guardasinfo(seguranca_spw)
-    guardasinfo(seguranca_spw_2)
-    guardasinfo(seguranca_spw_3)
-    guardasinfo(seguranca_spw_4)
-    guardasinfo(seguranca_spw_5)
-    guardasinfo(seguranca_spw_6)
-    guardasinfo(seguranca_spw_7)
-    guardasinfo(seguranca_spw_8)
-    guardasinfo(seguranca_spw_9)
-    guardasinfo(seguranca_spw_10)
-    local blip_carrof = AddBlipForEntity(carrof_spw)
-    SetBlipColour(blip_carrof, 1)
-    SetBlipRoute(blip_carrof, true)
-    SetBlipRouteColour(blip_carrof, 5)
-    TaskVehicleDriveToCoordLongrange(seguranca_spw, carrof_spw, 1847.96, 2608.26, 45.59, 16.0, 447, 1)
-    TaskVehicleDriveToCoordLongrange(seguranca_spw_3, carros_spw, 1847.96, 2608.26, 45.59, 16.0, 447, 1)
-    TaskVehicleDriveToCoordLongrange(seguranca_spw_7, carros_spw_2, 1847.96, 2608.26, 45.59, 16.0, 447, 1)
+    setupModelo(carf_hash)
+    setupModelo(security_cars_hash)
+    local carf_spw = CreateVehicle(carf_hash, -2959.50, 492.623, 14.81, 88.75, true, false)
+    local cars_spw = CreateVehicle(security_cars_hash, -2943.69, 492.69, 15.29, 88.75, true, false)  
+    local cars_spw_2 = CreateVehicle(security_cars_hash, -2971.292,493.226,15.318, true, false)
+    carsinfo(carf_spw)
+    carsinfo(cars_spw)
+    carsinfo(cars_spw_2)
+    setupModelo(security)
+    local security_spw = CreatePedInsideVehicle(carf_spw, 4, security, -1, true, false)
+    local security_spw_2 = CreatePedInsideVehicle(carf_spw, 4, security, 0, true, false)
+    local security_spw_3 = CreatePedInsideVehicle(cars_spw, 4, security, -1, true, false)
+    local security_spw_4 = CreatePedInsideVehicle(cars_spw, 4, security, 0, true, false)
+    local security_spw_5 = CreatePedInsideVehicle(cars_spw, 4, security, 1, true, false)
+    local security_spw_6 = CreatePedInsideVehicle(cars_spw, 4, security, 2, true, false)
+    local security_spw_7 = CreatePedInsideVehicle(cars_spw_2, 4, security, -1, true, false)
+    local security_spw_8 = CreatePedInsideVehicle(cars_spw_2, 4, security, 0, true, false)
+    local security_spw_9 = CreatePedInsideVehicle(cars_spw_2, 4, security, 1, true, false)
+    local security_spw_10 = CreatePedInsideVehicle(cars_spw_2, 4, security, 2, true, false)
+    SetEntityAsMissionEntity(security_spw, 0, 0) 
+    SetEntityAsMissionEntity(security_spw_2, 0, 0)
+    SetEntityAsMissionEntity(security_spw_3, 0, 0) 
+    SetEntityAsMissionEntity(security_spw_4, 0, 0) 
+    SetEntityAsMissionEntity(security_spw_5, 0, 0)
+    SetEntityAsMissionEntity(security_spw_6, 0, 0)
+    SetEntityAsMissionEntity(security_spw_7, 0, 0)
+    SetEntityAsMissionEntity(security_spw_8, 0, 0)
+    SetEntityAsMissionEntity(security_spw_9, 0, 0)
+    SetEntityAsMissionEntity(security_spw_10, 0, 0) 
+    guardsinfo(security_spw)
+    guardsinfo(security_spw_2)
+    guardsinfo(security_spw_3)
+    guardsinfo(security_spw_4)
+    guardsinfo(security_spw_5)
+    guardsinfo(security_spw_6)
+    guardsinfo(security_spw_7)
+    guardsinfo(security_spw_8)
+    guardsinfo(security_spw_9)
+    guardsinfo(security_spw_10)
+    local blip_carf = AddBlipForEntity(carf_spw)
+    SetBlipColour(blip_carf, 1)
+    SetBlipRoute(blip_carf, true)
+    SetBlipRouteColour(blip_carf, 5)
+    TaskVehicleDriveToCoordLongrange(security_spw, carf_spw, 1847.96, 2608.26, 45.59, 16.0, 447, 1)
+    TaskVehicleDriveToCoordLongrange(security_spw_3, cars_spw, 1847.96, 2608.26, 45.59, 16.0, 447, 1)
+    TaskVehicleDriveToCoordLongrange(security_spw_7, cars_spw_2, 1847.96, 2608.26, 45.59, 16.0, 447, 1)
+    hacking = false
     while true do
       local final = { 1847.96, 2608.26, 45.59 }
       Wait(0)
-      if GetEntityCoords(carrof_spw) == final  then
-        DeleteEntity(carrof_spw)
-      elseif GetEntityCoords(carros_spw) == final then
-        DeleteEntity(carros_spw)
-      elseif GetEntityCoords(carrof_spw_2) == final then
-        DeleteEntity(carros_spw_2)
+      if GetEntityCoords(carf_spw) == final  then
+        DeleteEntity(carf_spw)
+      elseif GetEntityCoords(cars_spw) == final then
+        DeleteEntity(cars_spw)
+      elseif GetEntityCoords(carf_spw_2) == final then
+        DeleteEntity(cars_spw_2)
       end
-      if GetVehicleBodyHealth(carrof_spw) < 5.0 then
+      if GetVehicleBodyHealth(carf_spw) < 5.0 then
         break
       end
-      dinheiro_tick(carrof_spw,carros_spw,carros_spw_2)
+      money_tick(carf_spw,cars_spw,cars_spw_2)
     end
   else
     print('Falha')
@@ -148,7 +149,7 @@ function mycb(successo, temporestante)
     DoScreenFadeIn(1000)
     Wait(1350)
     ClearPedTasks(ped)
-    hackeando = false
+    hacking = false
   end
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
@@ -165,9 +166,9 @@ end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- GUARDS
 -----------------------------------------------------------------------------------------------------------------------------------------
-function guardasinfo(inputPed)
+function guardsinfo(inputPed)
   SetPedShootRate(inputPed,  700)
-  AddArmourToPed(inputPed, GetPlayerMaxArmour(seguranca_spw)- GetPedArmour(seguranca_spw))
+  AddArmourToPed(inputPed, GetPlayerMaxArmour(security_spw)- GetPedArmour(security_spw))
   SetPedAlertness(inputPed, 3)
   SetPedAccuracy(inputPed, 81)
   SetEntityHealth(inputPed,  200)
@@ -190,7 +191,7 @@ end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- FINISHING UP THE CARS
 -----------------------------------------------------------------------------------------------------------------------------------------
-function carrosinfo(inputcarro)
+function carsinfo(inputcarro)
   RequestCollisionForModel(inputcarro)
   N_0x06faacd625d80caa(inputcarro)
   SetVehicleDoorsLocked(inputcarro , 7)
@@ -200,7 +201,7 @@ end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- PAYMENT AND CLEANING THE REST
 -----------------------------------------------------------------------------------------------------------------------------------------
-function dinheiro_tick(variavel, variavel_2, variavel_3)
+function money_tick(variavel, variavel_2, variavel_3)
   for i=0, 1, 1 do
     local flag = 0
     if GetVehicleDoorAngleRatio(variavel, 2) > .1 or GetVehicleDoorAngleRatio(variavel, 3) > .1 and flag == 0 then
@@ -209,14 +210,13 @@ function dinheiro_tick(variavel, variavel_2, variavel_3)
       local a,b,c = table.unpack(GetEntityCoords(variavel_2))
       local d,e,f = table.unpack(GetEntityCoords(variavel_3))
       local x,y,z = table.unpack(GetEntityCoords(variavel))
-      dinheiro_carrof = CreateAmbientPickup(GetHashKey("PICKUP_MONEY_SECURITY_CASE"), GetOffsetFromEntityInWorldCoords(variavel, 0.0, -5.0, -0.3001), 0, 300000) 
+      money_carf = CreateAmbientPickup(GetHashKey("PICKUP_MONEY_SECURITY_CASE"), GetOffsetFromEntityInWorldCoords(variavel, 0.0, -5.0, -0.3001), 0, 300000) 
       ClearAllBlipRoutes()
-      func.pagamento()
       Wait(9000)
       DeleteEntity(variavel)
       DeleteEntity(variavel_2)
       DeleteEntity(variavel_3)
-      hackeando = false
+      hacking = false
       break
     end
   end
